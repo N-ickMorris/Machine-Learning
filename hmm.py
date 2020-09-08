808a390e3c6972349d19e7f559610cf1422ef0f3
Nicholas J Morris <nicholasjmorris93@gmail.com>
2020-09-08 10:39:57 -0400

    Add files via upload

--- hmm.py
+++ hmm.py
@@ -5,7 +5,6 @@ Trains and tests a Hidden Markov Model on data
 @author: Nick
 """
 
-import numpy as np
 import pandas as pd
 from hmmlearn import hmm
 from plots import scatter_plot, pairs_plot
@@ -15,27 +14,25 @@ from plots import scatter_plot, pairs_plot
 # load dataset
 Y = pd.read_csv('Y time.csv').iloc[:,[0]]
 
+# difference the data by 1 period
+Y_diff = Y.diff().fillna(value=0)
+
 # split up the data into training and testing
-X = Y.values
+X = Y_diff.values
 size = int(len(X) * 0.8)
 train, test = X[0:size], X[size:len(X)]
+test2 = Y[size:len(Y)]
 
-# train and forecast one step ahead for the entire test set
+# train and forecast the entire test set
 history = [x for x in train]
-states = list()
-for t in range(len(test)):
-	model = hmm.GaussianHMM(n_components=4, covariance_type="full", n_iter=100)
-	model.fit(history)
-	obs = [test[t]]
-	output = model.predict(obs)
-	yhat = output[0]
-	history.append(obs[0])
-	states.append(yhat)
-	print('state=%f, observed=%f' % (yhat, obs[0][0]))
+model = hmm.GaussianHMM(n_components=4, covariance_type="full", n_iter=100)
+model.fit(history)
+states = model.predict(test)
 
 # collect states and actual values
-states = pd.DataFrame({"Actual": test.flatten(), 
-                       "State": np.array(states).flatten()}).reset_index()
+states = pd.DataFrame({"Actual": test2.to_numpy().flatten(),
+                       "Difference": test.flatten(),
+                       "State": states.flatten()}).reset_index()
 states.to_csv("hmm states.csv", index=False)
 
 # In[2]: Visualize the states
@@ -45,6 +42,9 @@ scatter_plot(data=states,
              x="index", y="Actual",
              color="State", title="Hidden Markov Model",
              legend=True, save=False)
-
-pairs_plot(data=states, vars=["index", "Actual"], 
+scatter_plot(data=states,
+             x="index", y="Difference",
+             color="State", title="Hidden Markov Model",
+             legend=True, save=False)
+pairs_plot(data=states, vars=["index", "Actual", "Difference"],
            color="State", title="Hidden Markov Model", save=False)
